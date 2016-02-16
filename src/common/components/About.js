cf59aa66e61f9f36f3a5f4344498c1f411bc85ce
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-02-15 21:06:08 -0800

    Fix server startup issue

---
+++ src/common/components/About.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+
+  render() {
+    return (
+      	<div className="posts">
+
+			    <h1 className="post-title">Why I coded this</h1>
+    			<p>Redux and React are rapidly developing code bases. I was having difficultly finding a simple boiler plate example to base my Redux app on. I decided to create my own stripped down version.</p>
+  		</div>
+
+    );
+  }
+}
+
+export default About;
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,95 @@
 var path = require('path');
 var webpack = require('webpack');
+var merge = require('merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    devtool: 'inline-source-map',
-    entry: [
-        'webpack-dev-server/client?http://127.0.0.1:8080/',
-        'webpack/hot/only-dev-server',
-        './app/components'
-    ],
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        modulesDirectories: ['node_modules', 'app'],
-        extensions: ['', '.js', '.jsx', '.scss']
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
-            },
-            {
-                test: /\.scss$/,
-                loaders: [
-                    'style',
-                    'css',
-                    'autoprefixer?browsers=last 3 versions',
-                    'sass?outputStyle=expanded'
-                ]
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                loaders: [
-                    'url?limit=8192',
-                    'img'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ],
-    devServer: {
-        hot: true,
-        proxy: {
-            '*': 'http://127.0.0.1:' + (process.env.PORT || 3000)
-        },
-        host: '127.0.0.1'
-    }
+var webpackConfig = {
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 };
+
+if (process.env.NODE_ENV === 'production') {
+
+  webpackConfig = merge(webpackConfig,{
+    devtool: "source-map",
+    entry : [
+      './src/client/index.js'
+    ],
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        include: __dirname
+      },
+      { test: /\.(png|jpg|gif|jpeg)$/, loader: 'url-loader?limit=8192'},
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap') }
+    ]},
+    plugins : [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      }),
+      new ExtractTextPlugin("app.css"),
+      new webpack.optimize.UglifyJsPlugin({minimize: true})
+    ]  
+  });
+
+}else{
+
+  webpackConfig = merge(webpackConfig,{
+    devtool: 'inline-source-map',
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        include: __dirname,
+        query: {
+          optional: ['runtime'],
+          stage: 2,
+          env: {
+            development: {
+              plugins: [
+                'react-transform'
+              ],
+              extra: {
+                'react-transform': {
+                  transforms: [{
+                    transform:  'react-transform-hmr',
+                    imports: ['react'],
+                    locals:  ['module']
+                  },
+                  {
+                    transform: 'react-transform-catch-errors',
+                    imports: ['react','redbox-react' ]
+                  }
+                ]}
+              }
+            }
+          }
+        }
+      },
+      { test: /\.(png|jpg|gif|jpeg)$/, loader: 'url-loader?limit=8192'},
+      { test: /\.css$/, loader: 'style-loader!css-loader' }
+    ]},
+    entry : [
+      'webpack-hot-middleware/client',
+      './src/client/index.js'
+    ],
+    plugins : [
+      new webpack.HotModuleReplacementPlugin()
+    ]  
+  });
+  
+}
+
+module.exports = webpackConfig;
