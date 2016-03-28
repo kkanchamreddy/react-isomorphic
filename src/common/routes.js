6d30419842c6a1199e323d0757733be8df39a90e
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-03-27 23:27:17 -0700

    revert the changes

--- src/common/routes.js
+++ src/common/routes.js
@@ -4,15 +4,15 @@ import React from "react";
 import App from "./containers/App";
 
 //Redux Smart
-//import CounterPage from "./containers/CounterPage";
+import CounterPage from "./containers/CounterPage";
 //import RedditPage from "./containers/RedditPage";
-//import TodoPage from "./containers/TodoPage";
-//import ImgurPage from "./containers/ImgurPage";
+import TodoPage from "./containers/TodoPage";
+import ImgurPage from "./containers/ImgurPage";
 
 //Redux Dumb
 import HomePage from "./components/Home";
-//import AboutPage from "./components/About";
-//import error404 from "./components/404";
+import AboutPage from "./components/About";
+import error404 from "./components/404";
 
 function lazyLoadComponent(component) {
   return function(location, cb) {
@@ -26,10 +26,12 @@ export default (
   <Route name="app" path="/" component={App}>
       <Route path="home" component={HomePage} />
       <Route path="reddit" getComponent={lazyLoadComponent('./containers/RedditPage')} />
-      <Route path="imgur" getComponent={lazyLoadComponent('./containers/ImgurPage')} />
-      <Route path="todo" getComponent={lazyLoadComponent('./containers/TodoPage')}  />
-      <Route path="counter" getComponent={lazyLoadComponent('./containers/CounterPage')}  />
-      <Route path="about" getComponent={lazyLoadComponent('./components/About')}  />
-      <Route path="*" getComponent={lazyLoadComponent('./components/404')} />
+      <Route path="imgur" component={ImgurPage} />
+      <Route path="todo" getComponent={(location, cb) => {
+            cb(null, TodoPage)
+      }} />
+      <Route path="counter" component={CounterPage} />
+      <Route path="about" component={AboutPage} />
+      <Route path="*" component={error404}/>
   </Route>
 );
